b1e3f185f0c15335dac6041373516f96b69f9832
Munkhtegsh Munkhbat <munkhtegsh.m@gmail.com>
2018-08-26 23:41:55 -0700

    Created Recipe and User models

--- server.js
+++ server.js
@@ -1,6 +1,44 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+require('dotenv').config({path: 'variables.env'});
+
+const Recipe = require('./models/Recipe');
+const User = require('./models/User');
+
+// Bring in GrapQL Express middleware
+const {graphiqlExpress, graphqlExpress} = require('apollo-server-express');
+const {makeExecutableSchema} = require('graphql-tools');
+
+const  {resolvers} = require('./resolvers');
+const {typeDefs} = require('./schema');
+
+// Create schema
+const schema = makeExecutableSchema({
+  typeDefs,
+  resolvers
+})
+
+// Connect to database
+mongoose  
+  .connect(process.env.MONGO_URI)
+  .then(() => console.log("DB connected"))
+  .catch(err => console.log("Error in MongoDB"));
+
+
+// Initialize application
 const app = express();
+
+app.use('/graphiql', graphqlExpress({endpointURL: '/graphql'}));
+
+// Connect schemas with GraphQL
+app.use('/graphql', graphqlExpress({
+  schema,
+  context: {
+    Recipe, 
+    User
+  }
+}))
 
 const PORT = process.env.PORT || 4444;
 app.listen(PORT, () => {
