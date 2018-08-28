16898ce385db9219c49f8e2946a1025754340c76
Munkhtegsh Munkhbat <munkhtegsh.m@gmail.com>
2018-08-27 23:09:52 -0700

    Created Queries folder, write first Apollo query

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,14 +1,25 @@
-import React, { Component } from 'react';
+import React from 'react';
 import './App.css';
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        Home
-      </div>
-    );
-  }
-}
+import { Query } from 'react-apollo';
+import { GET_ALL_RECIPES } from '../queries';
+
+const App = () => (
+  <div className="App">
+    <h1>Home</h1>
+
+    <Query query={GET_ALL_RECIPES}>
+      {({ data, loading, error }) => {
+        if (loading) return <div>Loading</div>
+        if (error) return <div>Error</div>
+        console.log(data);
+
+        return (
+          <p>Recipes</p>
+        )
+      }}
+    </Query>
+  </div>
+)
 
 export default App;
