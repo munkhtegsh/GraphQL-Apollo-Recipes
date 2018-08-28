0b686ef9308029e1ec40707bf452617c518d4f97
Munkhtegsh Munkhbat <munkhtegsh.m@gmail.com>
2018-08-27 23:23:38 -0700

    Add cors Middleware to Prevent Cross-Origin errors

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
+const cors = require('cors');
 require('dotenv').config({path: 'variables.env'});
 
 const Recipe = require('./models/Recipe');
@@ -28,6 +29,12 @@ mongoose
 
 // Initialize application
 const app = express();
+
+const corsOptions = {
+  origin: "http://localhost:3000",
+  credentials: true
+}
+app.use(cors(corsOptions));
 
 // Create GraphQL application
 app.use('/graphiql', graphiqlExpress({endpointURL: '/graphql'}));
