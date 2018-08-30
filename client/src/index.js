46176a8a2e5d775a94e3c210a33777a595687353
Munkhtegsh Munkhbat <munkhtegsh.m@gmail.com>
2018-08-29 23:38:05 -0700

    Add routing with react-router-dom

--- client/src/index.js
+++ client/src/index.js
@@ -1,17 +1,33 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import {BrowserRouter as Router, Route, Switch, Redirect} from 'react-router-dom';
 import './index.css';
 import App from './components/App';
+import Signin from './components/Auth/Signin';
+import Signup from './components/Auth/Signup';
 
 import ApolloClient from 'apollo-boost'; 
 import { ApolloProvider } from 'react-apollo';
 
 const client = new ApolloClient({
   uri: 'http://localhost:4444/graphql'
-})
+});
 
+const Root = () => (
+  <Router>
+    <Switch>
+      <Route path="/" exact component={App} />
+      <Route path="/signin" component={Signin} />
+      <Route path="/signup" component={Signup} />
+      
+      <Redirect to="/" />
+     </Switch>
+  </Router>
+)
+
+// Repaced app with Root 
 ReactDOM.render(
   <ApolloProvider client={client}>
-    <App />
+    <Root />  
   </ApolloProvider>,
 document.getElementById('root'));
