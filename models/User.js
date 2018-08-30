b12a9acccd2481ae982aefa2fbf6ed89205a6aac
Munkhtegsh Munkhbat <munkhtegsh.m@gmail.com>
2018-08-29 23:20:20 -0700

    Hash User Password with bcrypt

--- models/User.js
+++ models/User.js
@@ -1,5 +1,6 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
+const bcrypt = require('bcrypt');
 
 const UserSchema = new Schema({
   username: {
@@ -28,7 +29,22 @@ const UserSchema = new Schema({
     ref: 'Recipe'
   }
 
-
 });
+
+UserSchema.pre('save', function(next) {
+  if (!this.isModified('password')) {
+    return next();
+  }
+
+  bcrypt.genSalt(10, (err, salt) => {
+    if (err) return next(err);
+
+    bcrypt.hash(this.password, salt, (err, hash) => {
+      if (err) return next(err);
+      this.password = hash;
+      next();
+    })
+  })
+})
 
 module.exports = mongoose.model('user', UserSchema);
