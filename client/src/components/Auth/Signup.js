d51c8969ebbbf9a31302ae0fad221e388779472b
Munkhtegsh Munkhbat <munkhtegsh.m@gmail.com>
2018-08-30 18:48:05 -0700

    Add Form to Signup Component

--- client/src/components/Auth/Signup.js
+++ client/src/components/Auth/Signup.js
@@ -3,7 +3,20 @@ import React from 'react';
 class Signup extends React.Component {
   render() {
     return (
-      <div>Signup</div>
+      <div className="App">
+        <h2 className="App">Signup</h2>
+        <form className="form">
+          <input type="text" name="username" placeholder="Username" />
+          <br/>
+          <input type="email" name="email" placeholder="Email adress" />
+          <br/>
+          <input type="password" name="password" placeholder="Password" />
+          <br/>
+          <input type="password" name="passwordConfirmation" placeholder="Confirm Password" />
+          <br/>
+          <button type="submit" className="button-primary">Submit</button>
+        </form>
+      </div>
     )
   }
 }
