bb18b928d7f44788fdb5829301203c935b5fe75f
Munkhtegsh Munkhbat <munkhtegsh.m@gmail.com>
2018-08-30 18:58:29 -0700

    Manage Input state signup form

--- client/src/components/Auth/Signup.js
+++ client/src/components/Auth/Signup.js
@@ -1,18 +1,33 @@
 import React from 'react';
 
 class Signup extends React.Component {
+  state = {
+    username: '',
+    email: '',
+    password: '',
+    passwordConfirmation: ''
+  };
+
+  handleChange = (event) => {
+    const {name, value} = event.target;
+    this.setState({
+      [name]: value
+    })
+  }
+
   render() {
+    const {username, email, password, passwordConfirmation} = this.state;
     return (
       <div className="App">
         <h2 className="App">Signup</h2>
         <form className="form">
-          <input type="text" name="username" placeholder="Username" />
+          <input value={username} onChange={(event) => this.handleChange(event)} type="text" name="username" placeholder="Username" />
           <br/>
-          <input type="email" name="email" placeholder="Email adress" />
+          <input value={email} onChange={(event) => this.handleChange(event)} type="email" name="email" placeholder="Email adress" />
           <br/>
-          <input type="password" name="password" placeholder="Password" />
+          <input value={password} onChange={(event) => this.handleChange(event)} type="password" name="password" placeholder="Password" />
           <br/>
-          <input type="password" name="passwordConfirmation" placeholder="Confirm Password" />
+          <input value={passwordConfirmation} onChange={(event) => this.handleChange(event)} type="password" name="passwordConfirmation" placeholder="Confirm Password" />
           <br/>
           <button type="submit" className="button-primary">Submit</button>
         </form>
