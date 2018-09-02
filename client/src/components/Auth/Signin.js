e042bf9c41c13eec0aa26281b1ace6eeffec5b71
Munkhtegsh Munkhbat <munkhtegsh.m@gmail.com>
2018-09-01 23:15:30 -0700

    Implement Signin mutation on Client

--- client/src/components/Auth/Signin.js
+++ client/src/components/Auth/Signin.js
@@ -1,9 +1,67 @@
 import React from 'react';
+import {Mutation} from 'react-apollo';
+import {SIGNIN_USER} from '../../queries';
 
+const inititialState = {
+  username: '',
+  password: '',
+} 
 class Signin extends React.Component {
+  state = {...inititialState};
+
+  handleChange = (event) => {
+    const {name, value} = event.target;
+    this.setState({
+      [name]: value
+    })
+  }
+
+  clearState = () => {
+    this.setState({...inititialState})
+  }
+
+  handleSubmit = (event, signinUser) => {
+    event.preventDefault();
+    signinUser().then(data => {
+      console.log(data)
+    });
+
+    this.clearState();
+  }
+
+  validateForm = () => {
+    const {username, password } = this.state;
+    const isInvalid = !username || !password;
+    return isInvalid;
+  }
+
   render() {
+    const {username, password} = this.state;
     return (
-      <div>Signin</div>
+      <div className="App">
+        <h2 className="App">Signin</h2>
+        <Mutation mutation={SIGNIN_USER} variables={{ username, password }}>
+          {(signinUser, {data, loading, error}) => {
+            return (
+              <form className="form" onSubmit={event => this.handleSubmit(event, signinUser)}>
+              <input value={username} onChange={(event) => this.handleChange(event)} type="text" name="username" placeholder="Username" />
+              <br/>
+              <input value={password} onChange={(event) => this.handleChange(event)} type="password" name="password" placeholder="Password" />
+              <br/>
+              <button 
+                type="submit" 
+                className="button-primary" 
+                disabled={loading || this.validateForm()}
+              >
+                Submit
+              </button>
+
+              {error && <p>{error.message}</p>}
+            </form>
+            )
+          }}
+        </Mutation>
+      </div>
     )
   }
 }
